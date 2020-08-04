a1257f2ff6cbe9563871f5b9ba16c30361c4e3ed
beserge <bensergentanis@gmail.com>
2020-08-04 16:17:28 -0400

    additional filepath tweak for daisysp

--- app/app.js
+++ app/app.js
@@ -43,6 +43,10 @@ function gatherExampleData(fpath)
 function displayReadMe(fname)
 {
     var url = "https://raw.githubusercontent.com/electro-smith/" + self.data.sel_example.url + "/master"
+    if (self.data.sel_example.url == "DaisySP"){
+        url += "/examples"
+    }
+    
     fname   = fname.substring(5,fname.length-4);
     url     = url + fname + "/README.md";
     
